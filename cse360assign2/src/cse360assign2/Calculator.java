d782e90bff66e72944f2c6889a2806968a38bccd
gomme <gomme@DESKTOP-NB15H72>
2019-02-25 19:55:58 -0700

    Changes made to History method and JavaDoc Comments(Reupload)
    The Current Changes were made to the Calculator Class:
    
    	History Method:
    		- Added a Global variable to the class which allows us to keep
    		  track of what functions were being used through the main
    
    		- The getHistory method now returns a String that contains all
    		  previously used functions.
    
    	javaDoc:
    		- The documentation was updated to be more descriptive with the
    		  current standards of javaDoc.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,62 +1,98 @@
-
+/**
+ * @author Gage Gommels
+ * @ID   1212690087
+ * @Assignment 2
+ * @Description This is a simple program that allows a user to create a Calculator and then input their own desired values. They are
+ * able to retrieve the total and a history string of what functions they have used in the past.
+ */
 package cse360assign2;
 
+
+/**
+ * This class file contains 6 methods 4 being basic calculator functions (add, sub, mult, divide).
+ * getTotal will return the value currently held in total
+ * getHistory will return the string with all of the previous commands used in the main function for that specific calculator.
+ */
 public class Calculator {
 
 	private int total;
 	private String history;
 	
+	
+	
 	/**
-	 * This is the constructor for the calculator class.
-	 * It will be called upon in main when executing the code.
+	 * This is the constructor for the calculator class:
+	 * it contains the starting value for the total and the starting String for the history variable
+	 * its purpose is to create a calculator data type with the starting total = to 0 and history string = "0"
 	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 		history = "0";
 	}
 	
+	
+	
 	/**
-	 * this method will return the total
-	 * @return
+	 * This method will simply return the total
+	 * @return total:	Holds the total for the Calculator class
 	 */
 	public int getTotal () {
 		return total;
 	}
 	
+	
+	
 	/**
-	 * this method will be called upon when the program finds a + symbol in the input.
+	 * This method will take a integer input and add it to the total
+	 * It will also add a + symbol and the value to the history string
+	 * @param value:	This needs to be an integer
 	 */
 	public void add (int value) {
 		total = total + value;
 		history += " + " + value;
 	}
 	
+	
+	
 	/**
-	 * this method will be called upon when the program finds a - symbol in the input.
+	 * This method will subtract the given value from the total
+	 * It will also add a - symbol and the value to the history string
+	 * @param value:	This needs to be an integer
 	 */
 	public void subtract (int value) {
 		total = total - value;
 		history += " - " + value;
 	}
 	
+	
+	
 	/**
-	 * this method will be called upon when the program finds a * symbol in the input.
+	 * This method will multiply the total by the value given in the parameters
+	 * It also will add a * symbol and value to the history string.
+	 * @param value:	This needs to be an integer
 	 */
 	public void multiply (int value) {
 		total = total * value;
 		history += " * " + value;
 	}
 	
+	
+	
 	/**
-	 * this method will be called upon when the program finds a / symbol in the input.
+	 * This method will divide the total by the value given in the parameters
+	 * It also will add a / symbol and value to the history string.
+	 * @param value:	This needs to be an integer
 	 */
 	public void divide (int value) {
-		total = total/value;
+		total = total / value;
 		history += " / " + value;
 	}
 	
+	
+	
 	/**
-	 * this method will return the history of the last used functions
+	 * This method will return the history string
+	 * @return history string:	contains the string value of all previous used fuctions.
 	 */
 	public String getHistory () {
 		return history;

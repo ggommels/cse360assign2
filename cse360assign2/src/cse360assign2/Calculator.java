e49ae38bb4d129067e280d3be680bbc7e857d1a4
gomme <gomme@DESKTOP-NB15H72>
2019-02-25 18:58:51 -0700

    Initalize the code! First push!

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,58 @@
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * This is the constructor for the calculator class.
+	 * It will be called upon in main when executing the code.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * this method will return the total
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * this method will be called upon when the program finds a + symbol in the input.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * this method will be called upon when the program finds a - symbol in the input.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * this method will be called upon when the program finds a * symbol in the input.
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * this method will be called upon when the program finds a / symbol in the input.
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * this method will return the history of the last used functions
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
